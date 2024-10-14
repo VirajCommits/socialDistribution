4e5c789026abde5584b0b348ec167424d57178b8
pmehra14 <pmehra1@ualberta.ca>
2024-10-14 11:52:17 -0600

    node management u.s.2

--- backendApp/admin.py
+++ backendApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Author
 
-# Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('id','displayName', 'host', 'github','profileImage','page')
+    search_fields = ('displayName', 'id','github')
