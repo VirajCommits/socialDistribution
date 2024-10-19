532694debd1c14573b053e3ccef16168891d9558
VirajCommits <virajmurab567@yahoo.com>
2024-10-19 17:16:37 -0600

    Routing left

--- tealfrontend/src/router/index.js
+++ tealfrontend/src/router/index.js
@@ -1,27 +1,32 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router';
 import AuthorPosts from '@/components/AuthorPosts.vue';
 import CreatePost from '@/components/CreatePost.vue';
 import EditPost from '@/components/EditPost.vue';
 
-Vue.use(Router);
+const routes = [
+  {
+    path: '/posts/all',
+    name: 'AuthorPosts',
+    component: AuthorPosts,
+    props: route => ({ authorId: 'http://www.github.com' }), // Replace with dynamic authorId as needed
+  },
+  {
+    path: '/posts/create',
+    name: 'CreatePost',
+    component: CreatePost,
+  },
+  {
+    path: '/posts/edit/:id',
+    name: 'EditPost',
+    component: EditPost,
+    props: true, // Allows route params to be passed as props to the component
+  },
+];
 
-export default new Router({
-  routes: [
-    {
-      path: '/posts/all',
-      name: 'AuthorPosts',  
-      component: AuthorPosts,  
-    },
-    {
-      path: '/posts/create',
-      name: 'CreatePost',
-      component: CreatePost,
-    },
-    {
-      path: '/posts/edit/:id',
-      name: 'EditPost',
-      component: EditPost,
-    },
-  ],
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
 });
+
+export default router;
