6cfe5171b743a9d654956336c5f54818fbf951ea
DarrenKrz <daz@DKrizki>
2024-11-04 11:33:20 -0700

    added more documentation

--- backendApp/urls.py
+++ backendApp/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.views.generic import TemplateView
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -16,6 +17,7 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
