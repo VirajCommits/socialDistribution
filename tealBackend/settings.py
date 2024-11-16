b144d216211c27cdc568ede619b758469689a05c
ShikariRB <bhardwajrakshit23@gmail.com>
2024-11-16 02:45:53 -0700

    Fix CSRF and CORS settings

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -84,7 +84,7 @@ REST_FRAMEWORK = {
     ],
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",  # Frontend development server
     "http://localhost:8000",  # Backend development server
