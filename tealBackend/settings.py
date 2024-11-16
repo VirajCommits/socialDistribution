143b4db8547feed93f778d58f42405de1034e2c6
ShikariRB <bhardwajrakshit23@gmail.com>
2024-11-16 03:18:37 -0700

    Add CSRF trusted origins

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -92,6 +92,12 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://teal-rakshit-a972530cc317.herokuapp.com",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
+
 
 ROOT_URLCONF = "tealBackend.urls"
 
