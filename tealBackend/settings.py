9cc15c2376488f2f45fa424ca2520dae4d92bcc2
DarrenKrz <daz@DKrizki>
2024-11-18 14:07:19 -0700

    update

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 import django_heroku
 import dj_database_url
 
@@ -20,6 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Initialize environment variables
+env = environ.Env()
+
+# Reading the env file (env.production for production, .env for local development)
+environ.Env.read_env(os.path.join(BASE_DIR, 'env.production'))  # Or 'read_env()' without specifying for .env
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,23 +128,10 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL") != None:
-    # Running on Heroku
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True
-        )
-    }
-else:
-    # Running locally.
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    'default': env.db(),  # This automatically reads DATABASE_URL
+}
+
 
 
 # Password validation
