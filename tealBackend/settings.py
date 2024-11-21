a3b46189277194843b7c79ecdb8787d2d3186364
VirajCommits <virajmurab567@yahoo.com>
2024-11-21 13:47:54 -0700

    Impmented posts and authors when node are connected

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -94,13 +94,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:8081",  # Frontend development server
     "http://localhost:8000",  # Backend development server
-    "https://social-distribution-1-3adb84f120d9.herokuapp.com/",
+    "https://social-distribution-1-3adb84f120d9.herokuapp.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:8000",
-    "https://social-distribution-1-3adb84f120d9.herokuapp.com/",
+    "https://social-distribution-1-3adb84f120d9.herokuapp.com",
     "http://localhost:8081"
     "http://localhost:8000",
 ]
