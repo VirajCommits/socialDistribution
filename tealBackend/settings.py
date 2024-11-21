b63ab3dc4966b195b5f99e1c5955bf197f2f5417
VirajCommits <virajmurab567@yahoo.com>
2024-11-21 13:26:13 -0700

    Impmented posts and authors when node are connected

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 import django_heroku
 import dj_database_url
 
@@ -20,6 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Initialize environment variables
+env = environ.Env()
+
+# Reading the env file (env.production for production, .env for local development)
+environ.Env.read_env(os.path.join(BASE_DIR, 'env.production'))  # Or 'read_env()' without specifying for .env
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +48,6 @@ USER_APPROVAL_REQUIRED = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
-    "social-distribution-1-3adb84f120d9.herokuapp.com",
     ".herokuapp.com",
 ]
 
@@ -81,34 +87,23 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",  # Use JWT authentication
-        'rest_framework.authentication.SessionAuthentication',
-        'backendApp.authentication.NodeBasicAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
 }
 
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "http://localhost:8081",  # Frontend development server
+    "http://localhost:8080",  # Frontend development server
     "http://localhost:8000",  # Backend development server
-    "https://social-distribution-1-3adb84f120d9.herokuapp.com",
+    "https://*.herokuapp.com",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://127.0.0.1:8000",
-    "https://social-distribution-1-3adb84f120d9.herokuapp.com",
-    "http://localhost:8081",
+    "https://*.herokuapp.com",
+    "http://localhost:8080",
     "http://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
-
-WHITENOISE_ALLOW_ALL_ORIGINS = True
-WHITENOISE_MANIFEST_STRICT = False
-WHITENOISE_USE_FINDERS = True
-
 
 
 ROOT_URLCONF = "tealBackend.urls"
@@ -132,23 +127,10 @@ TEMPLATES = [
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if os.environ.get("DATABASE_URL") != None:
-    # Running on Heroku
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True
-        )
-    }
-else:
-    # Running locally.
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES = {
+    'default': env.db(),  # This automatically reads DATABASE_URL
+}
+
 
 
 # Password validation
