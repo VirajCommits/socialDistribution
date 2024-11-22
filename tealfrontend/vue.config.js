26e57ec5bc4d0d3eee574f5413698d3a88503944
ShikariRB <bhardwajrakshit23@gmail.com>
2024-11-21 23:24:04 -0700

    Pushing with viraj's changes

--- tealfrontend/vue.config.js
+++ tealfrontend/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
 
 module.exports = defineConfig({
-  publicPath: '/',
+  publicPath: '/static/vue/',
   transpileDependencies: true,
   productionSourceMap: false,
   filenameHashing: true,
