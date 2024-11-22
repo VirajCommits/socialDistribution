e31c2a4ad4a5d82b0dec9a84d7eb74af7d1163bd
Sanket Lamba <sanket@CoolSanky>
2024-11-22 15:55:12 -0700

    heroku push2

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -33,12 +33,16 @@ DEBUG = env('DEBUG', default=True)
 
 USER_APPROVAL_REQUIRED = True
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[
-    "127.0.0.1",
-    "localhost",
-    "social-distribution-1-3adb84f120d9.herokuapp.com",
-    ".herokuapp.com",
-])
+ALLOWED_HOSTS = env.list(
+    "ALLOWED_HOSTS",
+    default=[
+        "127.0.0.1",
+        "localhost",
+        "social-distribution-1-3adb84f120d9.herokuapp.com",
+        "social-sanket-603a86c4b610.herokuapp.com",
+        ".herokuapp.com",
+    ],
+)
 
 # Application definition
 
@@ -85,6 +89,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",  # Frontend development server
     "http://localhost:8000",  # Backend development server
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",  # Production domain
+    "https://social-sanket-603a86c4b610.herokuapp.com",
     "http://127.0.0.1:8000",  # Localhost alternative
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -92,6 +97,7 @@ CORS_ALLOW_CREDENTIALS = True
 # CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",
+    "https://social-sanket-603a86c4b610.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
