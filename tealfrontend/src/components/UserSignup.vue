1e1bf10d6d2d849301962176ee0c131e381a060d
pmehra14 <pmehra1@ualberta.ca>
2024-11-23 01:30:13 -0700

    heroku push

--- tealfrontend/src/components/UserSignup.vue
+++ tealfrontend/src/components/UserSignup.vue
@@ -310,7 +310,7 @@ export default {
       
       try {
         const response = await axios.post(
-          "service/api/signup/",
+          "/signup/",
           {
             username: this.username,
             email: this.email,
