3c79c2b03b58257525a69e8382687ea1a6f309b0
ShikariRB <bhardwajrakshit23@gmail.com>
2024-11-23 13:50:28 -0700

    Pushing changes to heroku

--- backendApp/views.py
+++ backendApp/views.py
@@ -2790,8 +2790,9 @@ def test_node_connection(request):
 
 @csrf_exempt
 @api_view(['POST', 'GET', 'DELETE'])
-@authentication_classes([NodeBasicAuthentication])
+# @authentication_classes([NodeBasicAuthentication])
 # @permission_classes([IsAuthenticatedOrNode])
+@permission_classes([AllowAny])
 def inbox_handler(request, author_serial):
     """
     Handles inbox activities for a given author. Supports POST (to add activities),
