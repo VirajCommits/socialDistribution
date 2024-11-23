cb733c8b59bbe66678d70d4d00b1bf34b24ea37b
pmehra14 <pmehra1@ualberta.ca>
2024-11-22 23:59:36 -0700

    heroku push

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = env.list(
         "social-distribution-1-3adb84f120d9.herokuapp.com",
         "social-sanket-603a86c4b610.herokuapp.com",
         "teal-rakshit-a972530cc317.herokuapp.com",
+        "teal-pranav-0e8aa7849ad7.herokuapp.com",
     ],
 )
 
@@ -91,6 +92,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",  # Production domain
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-sanket-603a86c4b610.herokuapp.com",
+    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "http://127.0.0.1:8000",  # Localhost alternative
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -100,6 +102,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",
     "https://social-sanket-603a86c4b610.herokuapp.com",
+    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
--- tealfrontend/src/main.js
+++ tealfrontend/src/main.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 // const baseURL = process.env.VUE_APP_API_BASE_URL || 'http://localhost:8000/service/api';
 const isProduction = window.location.hostname.includes('herokuapp.com');
 const baseURL = isProduction
-  ? 'https://teal-rakshit-a972530cc317.herokuapp.com/service/api'
+  ? 'https://teal-pranav-0e8aa7849ad7.herokuapp.com/'
   : 'http://127.0.0.1:8000/service/api';
 
 // Axios configuration
