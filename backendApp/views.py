700c71b2ef281ebddd1e589eb1745fe2c1419fca
ShikariRB <bhardwajrakshit23@gmail.com>
2024-11-23 14:57:38 -0700

    Pushing changes to heroku

--- backendApp/views.py
+++ backendApp/views.py
@@ -2790,9 +2790,9 @@ def test_node_connection(request):
 
 @csrf_exempt
 @api_view(['POST', 'GET', 'DELETE'])
-# @authentication_classes([NodeBasicAuthentication])
-# @permission_classes([IsAuthenticatedOrNode])
-@permission_classes([AllowAny])
+@authentication_classes([NodeBasicAuthentication])
+@permission_classes([IsAuthenticatedOrNode])
+# @permission_classes([AllowAny])
 def inbox_handler(request, author_serial):
     """
     Handles inbox activities for a given author. Supports POST (to add activities),
--- tealfrontend/src/components/ExploreAuthors.vue
+++ tealfrontend/src/components/ExploreAuthors.vue
@@ -274,15 +274,18 @@ export default {
             },
             object: targetAuthor  // The target author already has all required fields from get_full_data()
           };
+
+          const credentials = btoa(`${targetNode.username}:${targetNode.password}`);
+        
           // If the target host is found, send the follow request with credentials
           await axios.post(
             `${targethost}/service/api/authors/${targetUuid}/inbox/`,
             followActivity,
             {
               headers: {
-                'node-username': targetNode.username,
-                'node-password': targetNode.password,
-              },
+                'Authorization': `Basic ${credentials}`,
+                'Content-Type': 'application/json'
+              }
             }
           );
 
