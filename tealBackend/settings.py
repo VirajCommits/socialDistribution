30145491e98012e6b6fd48971d1e5ac2d1c0f5c5
ShikariRB <bhardwajrakshit23@gmail.com>
2024-11-23 18:28:25 -0700

    Pushing changes to heroku

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -80,7 +80,8 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",  # Use JWT authentication
-        "backendApp.NodeBasicAuthentication",
+        'rest_framework.authentication.SessionAuthentication',
+        'backendApp.authentication.NodeBasicAuthentication',
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
