09c3969cdc49ba6cdad7ae9d2164e321a2907748
VirajCommits <virajmurab567@yahoo.com>
2024-11-23 21:40:10 -0700

    pushing changes to heroku

--- backendApp/views.py
+++ backendApp/views.py
@@ -1023,28 +1023,20 @@ def send_data_to_remote_node(url, data):
     if not matched_node:
         raise ValueError("No matching remote node found for the provided URL.")
 
-    # Extract credentials from the matched node
-    username = matched_node.username
-    password = matched_node.password
+    # The endpoint is 'inbox/'
+    print("This is the data i got:(Viraj) " , data)
+    endpoint = 'authors/${}/inbox/'
 
-    if not username or not password:
-        raise ValueError(f"Username or password not found for node {matched_node.url}.")
-
-    # Set up headers with Basic Authentication
-    headers = {
-        'Authorization': f'Basic {username}:{password}',  # Use basic auth format
-    }
-
-    new_url = url + "inbox/"
-
-    try:
-        # Send the POST request to the remote node
-        response = requests.post(new_url, json=data, headers=headers)
-        response.raise_for_status()
-    except requests.RequestException as e:
-        raise RuntimeError(f"Failed to send data to {matched_node.url}: {e}")
+    # Send the POST request via make_node_request
+    response = make_node_request(
+        base_url=matched_node.url,
+        endpoint=endpoint,
+        method='POST',
+        data=data
+    )
 
     return response
+
 
 @swagger_auto_schema(
     method="POST",
