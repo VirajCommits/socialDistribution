5b6ccac689d4c718d1a4b43a56bb3212b0cd90f3
pmehra14 <pmehra1@ualberta.ca>
2024-11-24 03:30:53 -0700

    heroku push

--- tealfrontend/src/components/CreatePost.vue
+++ tealfrontend/src/components/CreatePost.vue
@@ -287,7 +287,7 @@ export default {
         console.log("CONNECTED NODES DATA:" , connected_nodes)
         console.log("TARGET HOST:" , targethost)
 
-        const targetNode = connected_nodes.find(node => node.url === targethost);
+        const targetNode = connected_nodes.find(node => node.url === host);
         console.log("TARGET NODE:" , targetNode)
         const credentials = btoa(`${targetNode.username}:${targetNode.password}`);
         console.log(payload)
