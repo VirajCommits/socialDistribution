f674782e7c03f4a2e63d1d30009bc30cb20ce341
VirajCommits <virajmurab567@yahoo.com>
2024-11-24 21:18:49 -0700

    pushing changes to heroku

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -40,9 +40,7 @@ ALLOWED_HOSTS = env.list(
         "localhost",
         "social-distribution-1-3adb84f120d9.herokuapp.com",
         "social-sanket-603a86c4b610.herokuapp.com",
-        "teal-rakshit-a972530cc317.herokuapp.com",
-        "teal-pranav-0e8aa7849ad7.herokuapp.com",
-        ".herokuapp.com",
+        "teal-rakshit-a972530cc317.herokuapp.com/",
     ],
 )
 
@@ -74,12 +72,12 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware"
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        'backendApp.authentication.NodeBasicAuthentication',
+        "rest_framework_simplejwt.authentication.JWTAuthentication",  # Use JWT authentication
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
@@ -93,8 +91,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",  # Production domain
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-sanket-603a86c4b610.herokuapp.com",
-    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
-    "https://michael-heroku-9aacc16677e5.herokuapp.com",
     "http://127.0.0.1:8000",  # Localhost alternative
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -104,27 +100,10 @@ CSRF_TRUSTED_ORIGINS = [
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",
     "https://social-sanket-603a86c4b610.herokuapp.com",
-    "https://michael-heroku-9aacc16677e5.herokuapp.com",
-    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
 ]
-
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-    'node-password',
-    'node-username',
-]
-
 
 # WhiteNoise Configuration (remove non-standard settings)
 STATIC_URL = "/static/"
