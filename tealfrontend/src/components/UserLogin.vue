3ab7ba6e27784c4efd0dd3add9ab0dd1a7b4bb7a
VirajCommits <virajmurab567@yahoo.com>
2024-11-24 18:40:11 -0700

    pushing changes to heroku

--- tealfrontend/src/components/UserLogin.vue
+++ tealfrontend/src/components/UserLogin.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div class="login-container">
     <div class="login-box">
@@ -61,6 +62,8 @@
 
 <script>
 import axios from "axios";
+import Cookies from 'js-cookie';
+
 
 export default {
   name: "UserLogin",
@@ -80,13 +83,20 @@ export default {
 
       console.log("Trying to log in")
 
+
       try {
+        const csrfToken = Cookies.get("csrftoken"); // Get CSRF token from cookies
+
         // Attempt to log in the user
         console.log("username and pass:" , this.username , this.password)
         const response = await axios.post("/login/", {
           username: this.username,
           password: this.password,
-        });
+        }, {
+  headers: {
+    "X-CSRFToken": csrfToken, // Include the CSRF token in the headers
+  },
+});
 
         // Store authentication tokens and user data
         const accessToken = response.data.access;
