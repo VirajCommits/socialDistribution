456fe70e48f385093959e61a3b5de06962b2bc9d
pmehra14 <pmehra1@ualberta.ca>
2024-11-24 22:32:36 -0700

    heroku push

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -39,6 +39,7 @@ ALLOWED_HOSTS = env.list(
         "127.0.0.1",
         "localhost",
         "social-distribution-1-3adb84f120d9.herokuapp.com",
+        "teal-pranav-0e8aa7849ad7.herokuapp.com",
         "social-sanket-603a86c4b610.herokuapp.com",
         "teal-rakshit-a972530cc317.herokuapp.com/",
     ],
@@ -89,6 +90,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8080",  # Frontend development server
     "http://localhost:8000",  # Backend development server
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",  # Production domain
+    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-sanket-603a86c4b610.herokuapp.com",
     "http://127.0.0.1:8000",  # Localhost alternative
@@ -100,6 +102,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",
     "https://social-sanket-603a86c4b610.herokuapp.com",
+    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
