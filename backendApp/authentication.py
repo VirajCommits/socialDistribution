88fd6228ec43e26f915941758d1078a619289bb3
VirajCommits <virajmurab567@yahoo.com>
2024-11-24 23:25:55 -0700

    pushing changes to heroku

--- backendApp/authentication.py
+++ backendApp/authentication.py
@@ -1,32 +1,54 @@
+# backendApp/authentication.py
+
 from rest_framework import authentication
 from rest_framework import exceptions
 from .models import RemoteNode
 
+class RemoteNodeUser:
+    """
+    A wrapper class to make RemoteNode compatible with Django's authentication system.
+    """
+    def __init__(self, node):
+        self.node = node
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def __getattr__(self, attr):
+        return getattr(self.node, attr)
+
 class NodeBasicAuthentication(authentication.BaseAuthentication):
+    """
+    Custom authentication class for RemoteNode using Basic Authentication.
+    """
     def authenticate(self, request):
         # Get credentials from header
         auth_header = request.META.get('HTTP_AUTHORIZATION', '')
         if not auth_header.startswith('Basic '):
-            return None
+            return None  # Return None to allow other authentication classes to attempt
 
         import base64
         try:
             # Decode base64 credentials
             auth_decoded = base64.b64decode(auth_header[6:]).decode('utf-8')
-            username, password = auth_decoded.split(':')
-            print("username is: ", username)
-            print("password is: ", password)
-        except:
+            username, password = auth_decoded.split(':', 1)
+            print("Username:", username)
+            print("Password:", password)
+        except (base64.binascii.Error, ValueError):
             raise exceptions.AuthenticationFailed('Invalid basic auth credentials')
 
         try:
-            print("going inside try:, username: ", username)
-            # Check if node exists and credentials match
-            all_nodes = RemoteNode.objects.all()
-            for node in all_nodes:
-                print("ALLL NODES:", f"ID: {node.id}, Username: {node.username}, URL: {node.url}, Active: {node.active}, Password: {node.password}")
+            print("Authenticating user:", username)
+            # Fetch the RemoteNode instance
             remotenode = RemoteNode.objects.get(username=username, password=password, active=True)
         except RemoteNode.DoesNotExist:
             raise exceptions.AuthenticationFailed('No such node exists')
 
-        return (remotenode, None)
+        # Wrap the RemoteNode instance
+        user = RemoteNodeUser(remotenode)
+        return (user, None)
