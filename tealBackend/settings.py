2696dca41d324854e92be5618e0b7ac77bf46789
VirajCommits <virajmurab567@yahoo.com>
2024-11-24 23:04:51 -0700

    pushing changes to heroku

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -82,7 +82,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "backendApp.permissions.IsAuthenticatedOrNode",
-        # "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.IsAuthenticated",
 
     ]
 }
