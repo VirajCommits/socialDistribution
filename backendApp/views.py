dd3009feaab389f93d8b052294b1b5ef68f987f0
VirajCommits <virajmurab567@yahoo.com>
2024-11-24 22:05:42 -0700

    pushing changes to heroku

--- backendApp/views.py
+++ backendApp/views.py
@@ -4646,6 +4646,8 @@ def sync_remote_authors(request):
                     endpoint=endpoint,
                 )
 
+                print("****************" , response)
+
                 if response.status_code == 200:
                     print(response)
                     data = response.authors.json()
