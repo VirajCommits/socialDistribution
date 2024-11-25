11ab64bc7fa74b5e585139fc6a019ec069484ae6
VirajCommits <virajmurab567@yahoo.com>
2024-11-24 22:34:20 -0700

    pushing changes to heroku

--- backendApp/views.py
+++ backendApp/views.py
@@ -3951,8 +3951,8 @@ def test_node_connection(request):
 )
 
 @csrf_exempt
-@authentication_classes([])
-@permission_classes([])
+@authentication_classes([AllowAny])
+@permission_classes([AllowAny])
 @api_view(['POST', 'GET', 'DELETE'])
 def inbox_handler(request, author_serial):
     """
