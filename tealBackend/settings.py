b87f42c9f0e6c385e575f00eba0ffaafeba1b975
Gordon Chiang <gchiang56@gmail.com>
2024-11-25 14:09:42 -0700

    sankety

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -42,6 +42,7 @@ ALLOWED_HOSTS = env.list(
         "social-sanket-603a86c4b610.herokuapp.com",
         "teal-rakshit-a972530cc317.herokuapp.com",
         "teal-pranav-0e8aa7849ad7.herokuapp.com",
+        "teal-darrenkrz-c0d7276a7808.herokuapp.com",
         "project-teal-1-2b076456090f.herokuapp.com",
         ".herokuapp.com",
     ],
@@ -98,6 +99,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://social-sanket-603a86c4b610.herokuapp.com",
     "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "https://project-teal-1-2b076456090f.herokuapp.com",
+    "https://teal-darrenkrz-c0d7276a7808.herokuapp.com",
     "http://127.0.0.1:8000",  # Localhost alternative
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -109,6 +111,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://social-sanket-603a86c4b610.herokuapp.com",
     "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "https://project-teal-1-2b076456090f.herokuapp.com",
+    "https://teal-darrenkrz-c0d7276a7808.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
