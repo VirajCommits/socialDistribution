e6cf307330b3aa813e25bec80d1718709294c375
VirajCommits <virajmurab567@yahoo.com>
2024-11-26 14:04:36 -0700

    heroku

--- backendApp/views.py
+++ backendApp/views.py
@@ -4860,6 +4860,7 @@ def construct_comment_likes_data(comment):
     return likes_data
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def send_follow_request_to_remote_authors(request, author_serial):
     """
     Send a follow request to an author on a connected remote node.
