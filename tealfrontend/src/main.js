f4348f98ef35f8e1f1da65f20ad963a0e49df170
pmehra14 <pmehra1@ualberta.ca>
2024-11-26 21:49:48 -0700

    fix

--- tealfrontend/src/main.js
+++ tealfrontend/src/main.js
@@ -28,8 +28,7 @@ axios.interceptors.request.use(
   config => {
     const token = localStorage.getItem('token'); // Get the token from localStorage
     if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`; // Set the Authorization header
-      // console.log('Token found and added to request');
+      console.log('Token found and added to request');
     } else {
       // console.log('No token found in localStorage');
       localStorage.removeItem('token');
