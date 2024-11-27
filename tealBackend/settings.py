85f450aaf901a97dc803d750e1e9d1e946f42f16
VirajCommits <virajmurab567@yahoo.com>
2024-11-26 20:17:06 -0700

    inspecting auth issues

--- tealBackend/settings.py
+++ tealBackend/settings.py
@@ -90,7 +90,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",  # Production domain
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-sanket-603a86c4b610.herokuapp.com",
-    "https://teal-pranav-0e8aa7849ad7.herokuapp.com"
+    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "http://127.0.0.1:8000",  # Localhost alternative
 ]
 CORS_ALLOW_CREDENTIALS = True
@@ -100,6 +100,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://teal-rakshit-a972530cc317.herokuapp.com',
     "https://social-distribution-1-3adb84f120d9.herokuapp.com",
     "https://social-sanket-603a86c4b610.herokuapp.com",
+    "https://teal-pranav-0e8aa7849ad7.herokuapp.com",
     "http://localhost:8080",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
