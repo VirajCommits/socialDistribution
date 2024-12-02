5c29a66a8730da42662625abc8a90ad869fc22a9
DarrenKrz <daz@DKrizki>
2024-12-01 18:48:20 -0700

    heroku

--- tealfrontend/src/components/ProfilePage.vue
+++ tealfrontend/src/components/ProfilePage.vue
@@ -249,9 +249,9 @@ export default {
 
         // Assuming `response.data.following` contains the list of authors you're following
         this.stats = {
-          following: response.data.following.length || 0, // Count the following list
-          followers: response.data.followers.length || 0, // Get followers count
-          friends: response.data.friends.length || 0, // Get friends count
+          following: response.data.following || 0, // Count the following list
+          followers: response.data.followers || 0, // Get followers count
+          friends: response.data.friends || 0, // Get friends count
         };
       } catch (error) {
         console.error("Error fetching stats:", error);
