4c099a2dc8ad47919fc9367532ee57df8bfb65ac
DarrenKrz <daz@DKrizki>
2024-12-08 15:03:18 -0700

    working on like button

--- backendApp/views.py
+++ backendApp/views.py
@@ -4154,7 +4154,7 @@ def inbox_handler(request, author_serial):
                     like_id = like_data.get('id', str(uuid.uuid4()))  # Generate a UUID if not provided
 
                     # Parse and validate the target
-                    target_id = like_data.get('object', '').rstrip('/')
+                    target_id = like_data.get('object', None)
                     print("PRINTING TARGET ID:", target_id)
                     if '/posts/' in target_id:
                         # Like is for a Post
